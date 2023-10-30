db9c8f29be6f42829096dce3d2842ff7bbb9a9df
LostLuma <lilly@lostluma.net>
2023-10-30 18:59:15 +0100

    Stop non-player entities from dying during thunderstorms

--- src/main/java/net/lostluma/luma_mode/mixin/LightningBoltEntityMixin.java
+++ src/main/java/net/lostluma/luma_mode/mixin/LightningBoltEntityMixin.java
@@ -1,5 +1,7 @@
 package net.lostluma.luma_mode.mixin;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.living.player.PlayerEntity;
 import net.minecraft.entity.weather.LightningBoltEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,5 +19,20 @@ public class LightningBoltEntityMixin {
 	)
 	private boolean never(World world, int x, int y, int z, int range) {
 		return false;
+	}
+
+	/**
+	 * Stop non-player entities from receiving lightning damage, as it just makes the world empty over time.
+	 */
+	@Redirect(
+		method = "tick",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;onLightningStrike(Lnet/minecraft/entity/weather/LightningBoltEntity;)V")
+	)
+	private void onLightningStrike(Entity entity, LightningBoltEntity lightning) {
+		if (entity instanceof PlayerEntity) {
+			entity.onLightningStrike(lightning);
+		}
 	}
 }
